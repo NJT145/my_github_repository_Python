f51c6aa7a13ea82d28f254b95cbc27e368afd50a
Nejat Günaydın <njt145@gmail.com>
2017-05-10 14:10:04 +0300

    blablabla

--- CS_362_01/Assignment4 Part2/name_surname.py
+++
@@ -1,9 +0,0 @@
-def emm_no_pruning(grid_config, depth_limit):
-    ### your code here ###
-
-
-def emm_ab_pruning(grid_config, depth_limit):
-    #### your code here ###
-    
-def approximate_q_learning(grid_config):
-    #### your code here ###
---
+++ CS_372_01/Assignments/Nejat_Gunaydin_Assignment3/Nejat_Gunaydin_assignment3.py
@@ -0,0 +1,6 @@
+#-*- coding: utf-8 -*-
+
+"""
+Student name-surname-ID : Nejat Günaydın - 213861517
+
+"""
