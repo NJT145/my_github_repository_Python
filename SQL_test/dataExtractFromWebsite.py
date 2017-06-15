f845113cc8ced75f7af6d742a2f0d1633a016e1a
Nejat Günaydın <njt145@gmail.com>
2017-06-15 17:02:06 +0300

    Merge remote-tracking branch 'origin/master'

--- SQL_test/dataExtractFromWebsite.py
+++ SQL_test/dataExtractFromWebsite.py
@@ -3,6 +3,7 @@
 import requests
 import pandas as pd
 
+
 def webDataToCsv1(url, csv_name):
     df = webDataToDF(url)
     df.to_csv(csv_name)
@@ -14,6 +15,7 @@ def test1():
     csv_name1 = 'my data.csv'
     df1 = webDataToCsv1(url1, csv_name1)
     print df1
+
 
 def webDataToDF(url):
     html = requests.get(url).content
@@ -29,4 +31,28 @@ url2 = 'http://www.igdas.istanbul/AdresVeTelefonlar?id=233&lang=tr&sc=4'
 csv_name2 = 'AdresVeTelefonlar.csv'
 df = webDataToDF(url2)
 df.drop(1, axis=1, inplace=True)
-print df
+keys = []
+table_dict = {}
+last_key = None
+for line in df[0]:
+    if len(line) < 40:
+        last_key = line
+        table_dict[line] = []
+        keys.append(line)
+    else:
+        table_dict[last_key].append(line)
+
+resultTable_dict = {u"Bina Türü":{}, u"Başlık":{}, u"Adres":{}, u"Telefon":{}, u"Faks":{}}
+lastIndex = 0
+for key in keys:
+    for index in range(len(table_dict[key])):
+        row = lastIndex + index
+        if index == (len(table_dict[key]) - 1):
+            lastIndex = row
+
+
+
+for key in keys:
+    print "##Bina Türü## ", key
+    for lineIndex in range(len(table_dict[key])):
+        print "##Başlık## ", table_dict[key][lineIndex].split("ADRES :")[0], "##Adres## ", table_dict[key][lineIndex].split("ADRES :")[1].split("TELEFON :")[0], "##TEL##", table_dict[key][lineIndex].split("ADRES :")[1].split("TELEFON :")[1].split("Fax: ")[0], "##Fax##", table_dict[key][lineIndex].split("ADRES :")[1].split("TELEFON :")[1].split("Fax: ")[1]
