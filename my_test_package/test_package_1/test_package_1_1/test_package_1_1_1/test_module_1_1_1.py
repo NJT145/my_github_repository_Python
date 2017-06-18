5fb231cc11b47fe9df20b41339b420a7c89a6f42
Nejat Günaydın <njt145@gmail.com>
2017-06-18 19:45:33 +0300

    one more step

--- my_test_package/test_package_1/test_package_1_1/test_package_1_1_1/test_module_1_1_1.py
+++
@@ -1,7 +0,0 @@
-print("test_module_1_1_1")
-
-class test111(object):
-    def __init__(self):
-        print("test111 init")
-    def testFunc(self):
-        print("test111 function")
--- my_test_package/test_package_1/test_package_1_1/test_package_1_1_2/test_module_1_1_2.py
+++
@@ -1,7 +0,0 @@
-print("test_module_1_1_2")
-
-class test112(object):
-    def __init__(self):
-        print("test112 init")
-    def testFunc(self):
-        print("test112 function")
