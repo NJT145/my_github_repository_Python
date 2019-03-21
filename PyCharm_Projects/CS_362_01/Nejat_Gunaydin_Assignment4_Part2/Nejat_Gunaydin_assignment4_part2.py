58b3252351a45d6041131c9b16368dc502fb285f
Nejat Günaydın <njt145@gmail.com>
2019-03-21 23:20:22 +0300

    ...

--- PyCharm_Projects/CS_362_01/Nejat_Gunaydin_Assignment4_Part2/Nejat_Gunaydin_assignment4_part2.py
+++ PyCharm_Projects/CS_362_01/Nejat_Gunaydin_Assignment4_Part2/Nejat_Gunaydin_assignment4_part2.py
@@ -1,9 +1,12 @@
 def emm_no_pruning(grid_config, depth_limit):
     ### your code here ###
+    pass
 
 
 def emm_ab_pruning(grid_config, depth_limit):
     #### your code here ###
+    pass
     
 def approximate_q_learning(grid_config):
     #### your code here ###
+    pass
--- PyCharm_Projects/IntroductionToProgramming_ENGR105/MP1/nejat_gunaydin.py
+++ PyCharm_Projects/IntroductionToProgramming_ENGR105/MP1/nejat_gunaydin.py
@@ -1,12 +1,27 @@
 
-
-
-def bankApp():
+def bank_app():
+    # user1 info
     user1_name = "Ahmet"
     user1_password = "1234"
+    user1_currency = 0
+    # user1 info
     user2_name = "Zeyney"
     user2_password = "4321"
+    user2_currency = 0
+    # Variables
+    login = False
+    login_user1 = False
+    login_user2 = False
+    welcome_label = "--- Welcome to SEHIR Bank V.0.1 ---"
+
+    while True:
+        if login:
+            pass
+        else:
+            pass
+        break
 
     pass
 
-bankApp()
+
+bank_app()
